2bf2be51e09128ee6358f7cbc9e71141a2e5f589
Luis Raul Espinoza Barboza <respinozabarboza@gmail.com>
2020-04-01 14:21:32 -0700

    add csv utility with csv helper

---
+++ CVSUtility/CSVFormat.cs
@@ -0,0 +1,19 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CVSUtility
+{
+	internal class CSVFormat
+	{
+        public string Requestor { get; set; }
+        
+        public DateTime? CreatedOn { get; set; }
+        [Name("FIA Number")]
+        public string FIANumber { get; set; }
+        
+        [Name("Impact Value")]
+        public decimal? ImpactValue { get; set; }
+    }
+}
---
+++ CVSUtility/Program.cs
@@ -0,0 +1,56 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace CVSUtility
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				using (var reader = new StreamReader(@"C:\Users\1421574\Downloads\gdl_report.csv", System.Text.Encoding.Default))
+				using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+				{
+					csv.Configuration.Delimiter = "	";
+					var good = new List<CSVFormat>();
+					var bad = new List<string>();
+					var isRecordBad = false;
+					csv.Configuration.BadDataFound = context =>
+					{
+						isRecordBad = true;
+						bad.Add(context.RawRecord);
+					};
+					var records = csv.GetRecords<CSVFormat>().ToList();
+					Console.WriteLine(JsonConvert.SerializeObject(records));
+					
+					Console.ReadLine();
+				}
+			}
+			catch (UnauthorizedAccessException e)
+			{
+				throw new Exception(e.Message);
+			}
+			catch (FieldValidationException e)
+			{
+				throw new Exception(e.Message);
+			}
+			catch (CsvHelperException e)
+			{
+				throw new Exception(e.Message);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				Console.ReadLine();
+				throw new Exception(e.Message);
+			}
+			
+		}
+	}
+}
